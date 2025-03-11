2e5c2e9e080e0e3c34e3ff6e645032b58835db25
jinnybusock <jinnybusock@gmail.com>
2025-03-11 20:16:57 +0900

    price

--- week02.py
+++ week02.py
@@ -1,11 +1,16 @@
 # 1) Ice Americano : 2000 2) Latte : 3000
-menu = input("1) Ice Americano  2) Latte  3) Exit : ")
+prices= [2000, 3000]
 
-if menu== "1":
-    print("You ordered Ice Americano! Price : 2000 won. Thank you")
+while True:
+    menu = input(f"1) Ice Americano {prices[0]} 2) Latte {prices[1]}  3) Exit : ")
 
-elif menu== "2":
-    print("You ordered Latte! Price : 3000 won. Thank you")
+    if menu== "1":
+        print(f"You ordered Ice Americano! Price : {prices[0]} won. Thank you")
 
-else:
-    print("You finish order")
+    elif menu== "2":
+        print(f"You ordered Latte! Price : {prices[1]} won. Thank you")
+
+    else:
+        print("You finish order")
+
+    break
