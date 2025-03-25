bc9e40a2445e32fd6e37398ad0c15c178a296cf8
jinnybusock <jinnybusock@gmail.com>
2025-03-25 18:08:50 +0900

    week04 v1.7 exception handling

---
+++ week04.py
@@ -0,0 +1,52 @@
+#drinks= ["Ice Americano", "Latte"]
+#prices= [2000, 3000]
+# amounts= [0, 0]
+#amounts= [0]* len(drinks)
+#total_price= 0
+
+drinks= ["Ice Americano", "Latte", "Watermelon juice"]
+prices= [2000, 3000, 4900]
+amounts= [0] * len(drinks)
+total_price= 0
+#menu_lists= ""
+
+def order_process(idx: int):
+    """
+    음료 주문 디스플레이 및 누적금액 계산과 수량 처리 함수
+    :param idx: list's index num
+    """
+    global total_price
+    print(f"You ordered {drinks[idx]}! Price : {prices[idx]} won. Thank you")
+    total_price = total_price + prices[idx]
+    amounts[idx] = amounts[idx] + 1
+
+menu_lists= " ".join([f"{k+1}) {drinks[k]} {prices[k]} won " for k in range(len(drinks))])
+menu_lists+= f"{len(drinks)+ 1}) Exit: "
+
+# help(abs)
+# help(len)
+# help(order_process)
+
+while True:
+    try:
+        menu = int(input(menu_lists))
+        if len(drinks) >= menu >= 1:
+            order_process(menu - 1)
+
+        elif menu == len(drinks) + 1:
+            print("You finish order")
+            break
+
+        else:
+            print(f"{menu} menu is invalid. Please try again.")
+    except ValueError as err:
+        print(f"Cannot find menu number. Please put valuable number\n{err}")
+
+
+print("Product Price Amount Subtotal")
+
+for i in range(len(drinks)):
+    if amounts[i] > 0:
+        print(f"{drinks[i]} {prices[i]} x {amounts[i]} {prices[i] * amounts[i]}")
+
+print(f"Total price : {total_price}")
