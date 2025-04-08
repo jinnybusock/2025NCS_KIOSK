b46d5ae22c717334dfeff450166f2ff6c23a9078
jinnybusock <jinnybusock@gmail.com>
2025-04-08 18:38:05 +0900

    week06 v.2.5

--- week06.py
+++ week06.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     menu_prices = [2000, 3000, 4900]
 
     menu = kiosk.Menu(menu_drinks, menu_prices)
-    order_processor = kiosk.OrderProcessor() # Menu 객체를 생성하지 않음
+    order_processor = kiosk.OrderProcessor() # has-a, aggregation
     order_processor.run(menu) # run 메서드에 Menu 객체를 전달
